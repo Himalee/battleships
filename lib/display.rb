d0d81c3e480f67fb59e5aa835979f413fb0f7abd
Himalee <himalee.tailor@gmail.com>
2018-06-29 15:01:27 +0100

    refactors present_inital_board method and displays messages

--- lib/display.rb
+++ lib/display.rb
@@ -2,20 +2,65 @@ require_relative "console"
 
 class Display
 
-  def initialize(console)
+  def initialize(console = Console.new, message)
     @console = console
+    @messages = message
   end
 
-  def present_board(board)
+  def welcome
+    @console.present(@messages.welcome_message)
+  end
+
+  def user_input_coordinate
+    @console.receive
+  end
+
+  def user_input_row
+    @console.present(@messages.prompt_for_row)
+  end
+
+  def user_input_column
+    @console.present(@messages.prompt_for_column)
+  end
+
+  def present_initial_board(board)
+    present_header(board)
+    board.each do |letter, cells|
+      each_row = cells.map do |cell|
+        reveal(cell)
+      end
+    rows = each_row.join(" ")
+    @console.present("#{letter} #{rows}")
+    end
+  end
+
+  def reveal(cell)
+    if cell != @messages.miss_mark && cell != @messages.hit_mark
+      @messages.board_mark
+    else
+      cell
+    end
+  end
+
+  def present_header(board)
     columns = (0..board.count).to_a
     numbers = columns.join(" ")
     @console.present("#{numbers}")
-    board.each do |key, value|
-      each_row = value.map do |number|
-        "."
-      end
-      dots = each_row.join(" ")
-      @console.present("#{key} #{dots}")
-    end
+  end
+
+  def present_winning_message
+    @console.present(@messages.winning_message)
+  end
+
+  def present_retry_message
+    @console.present(@messages.retry_message)
+  end
+
+  def miss
+    @messages.miss_mark
+  end
+
+  def hit
+    @messages.hit_mark
   end
 end
--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -1,14 +1,38 @@
 require "display"
+require "messages"
 
 describe Display do
   context "board" do
     it "displays detailed grid" do
       output = StringIO.new
+      messages = Messages.new
       console = Console.new(output)
-      display = Display.new(console)
+      display = Display.new(console, messages)
       board = {"A"=>[1, 2, 3], "B"=>[1, 2, 3], "C"=>[1, 2, 3]}
-      display.present_board(board)
+      display.present_initial_board(board)
       expect(output.string).to eql("0 1 2 3\nA . . .\nB . . .\nC . . .\n")
+    end
+
+    it "displays detailed grid skipping marks (with X)" do
+      output = StringIO.new
+      messages = Messages.new
+      console = Console.new(output)
+      display = Display.new(console, messages)
+      mark = messages.hit_mark
+      board = {"A"=>[1, 2, mark], "B"=>[1, 2, 3], "C"=>[1, 2, 3]}
+      display.present_initial_board(board)
+      expect(output.string).to eql("0 1 2 3\nA . . X\nB . . .\nC . . .\n")
+    end
+
+    it "displays detailed grid skipping marks (with O)" do
+      output = StringIO.new
+      messages = Messages.new
+      console = Console.new(output)
+      display = Display.new(console, messages)
+      mark = messages.miss_mark
+      board = {"A"=>[1, 2, mark], "B"=>[1, 2, 3], "C"=>[1, 2, 3]}
+      display.present_initial_board(board)
+      expect(output.string).to eql("0 1 2 3\nA . . O\nB . . .\nC . . .\n")
     end
   end
 end
